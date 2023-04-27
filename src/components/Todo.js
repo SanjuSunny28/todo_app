b2886b9d202a3490e073be28df18615d0fe7f4ce
SanjuSunny28 <sanjusunny137@gmail.com>
2023-04-27 18:00:56 +0100

    first commit

---
+++ src/components/Todo.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react'
+import './ToDo.css'
+
+import { MdIncompleteCircle } from "react-icons/md";
+import { AiFillEdit ,AiFillDelete} from "react-icons/ai";
+
+const Todo = () => {
+    const [todo, setToDo] = useState ('')
+    const[todos, setToDos] = useState([])
+    const [editId,setEditId] = useState (0)
+
+    const handlerSubmit = (e) => {
+        e.preventDefault();
+    }
+
+ 
+    const addToDO =()=> {
+        setToDos([...todos,{list :todo ,id:Date.now(),status:false}])
+        setToDo("   ")
+    }
+
+    const onDelete = (id) =>{
+        setToDos(todos.filter((item) => item.id !== id));
+    }
+
+   
+  
+   
+  return (
+    <div className='container'>
+        <h2>ToDo App</h2>
+
+        <div>
+            <form className='form' onSubmit={handlerSubmit}>
+                <input type='text' value={todo} placeholder='Enter your todo...' onChange={(e)=>{setToDo(e.target.value)}} />
+                <button onClick={addToDO}  className='btn-add'>Add</button>
+            </form>
+
+            <ul className='todo-ul'>
+                {todos.map((item) =>( 
+                    <li className='todo-list' id={item.status ? "list-id" : " "} >{item.list}
+                    
+                    
+                    <AiFillDelete onClick={()=>onDelete(item.id)} className='delete-btn'/> 
+                    </li> 
+
+                ) )}
+                
+                
+               
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Todo
